8f2d0a4ccbfeef14127e26ba36f358282c49c20f
AarthiIyasamy <31529851+AarthiIyasamy@users.noreply.github.com>
2017-09-01 11:16:33 +0530

    Create ch.java

---
+++ ch.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class ch
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+char c='d';
+if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+{
+System.out.println(c+"is an Alphabet");
+}
+else
+{
+System.out.println(c+"is not an Alphabet");
+}
+}}
