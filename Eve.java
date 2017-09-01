18460cfe0078c9893d39a0e9c93f7be2e8400680
AarthiIyasamy <31529851+AarthiIyasamy@users.noreply.github.com>
2017-09-01 10:55:16 +0530

    Create Eve.java

---
+++ Eve.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Arrsort
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter any Number:");
+int n=s.nextInt();
+if(n%2==0)
+{
+System.out.println(n+" is Even");
+}
+else
+{
+System.out.println(n+" is Odd");
+}
+}
+}
