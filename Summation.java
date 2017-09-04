27eca899d9258e544144f3198ece1502d4d9852e
AarthiIyasamy <31529851+AarthiIyasamy@users.noreply.github.com>
2017-09-04 21:33:40 +0530

    Create Summation.java

---
+++ Summation.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Summation {
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a;
+		a=(((n)*(n+1))/(2));
+		System.out.println(a);
+	
+	}
+
+}
