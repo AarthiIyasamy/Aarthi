242caa8e5df0629ec51ff9a65e5ccf5f5907ab5a
AarthiIyasamy <31529851+AarthiIyasamy@users.noreply.github.com>
2017-09-05 21:40:41 +0530

    Create Concate.java

---
+++ Concate.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Concate
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter any two Strings:");
+String a=s.nextLine();
+String b=s.nextLine();
+String c=a+b;
+System.out.println(c);
+}
+}
